645c11baab0430197e3934660d0ffb0f82f24657
Noah Broyles <noah@javamate.net>
2021-11-22 16:26:15 -0500

    Add documentation

--- su_dentials.py
+++ su_dentials.py
@@ -28,6 +28,11 @@ class CredentialOverrideError(Exception):
 
 class Credentials:
     def __init__(self, password: str = None, credentials_file=CREDS_LOCATION):
+        """
+Create a Credentials instance.
+        :param password: Specifies the decryption password to read the credentials with
+        :param credentials_file: Specifies the unencrypted credentials file to base off of (Defaults to '/var/secure/creds.json')
+        """
         self._creds_path = credentials_file
         self._locked_path = f"{credentials_file}.robo"
         self._salt_file = '/var/secure/robocrypt.salt'
@@ -39,18 +44,34 @@ class Credentials:
         self.__store = Dict()
 
     def state(self):
+        """
+Get the state of the credentials file: 'locked' or 'unlocked' if the creds file is unencrypted or decrypted
+        :return: None
+        """
         if os.path.exists(self._creds_path) and not os.path.exists(self._locked_path):
             return 'unlocked'
         else:
             return 'locked'
 
     def set_password(self, new_password: str):
+        """
+Set's a password for the credentials manager to use when locking and unlocking the system files.
+If a password has been set already, you will be asked to use change_password() instead.
+        :param new_password: The password to set.
+        :return: None
+        """
         if self.__password is None:
             self.__password = new_password
         else:
             print('please use change_password instead.')
 
     def change_password(self, old_password: str, new_password: str):
+        """
+Changes the credentials manager password from the old password to a new password.
+        :param old_password: The old password that the credentials are encrypted with
+        :param new_password: The password to change to
+        :return: None
+        """
         try:
             self.unlock(password=old_password)
             self.__password = new_password
@@ -59,14 +80,28 @@ class Credentials:
             raise InvalidPassword('The old password specified is not correct.')
 
     def forget_password(self):
+        """
+Forgets the local password so that in order to lock or unlock you must specify a password.
+        :return:
+        """
         self.__password = None
 
     def lock(self, password: str = None):
+        """
+Encrypts the credentials file on disk
+        :param password: The password to use (Will use the default pw for the current object unless a different on is specified)
+        :return: None
+        """
         if self.state() == 'locked':
             return
         robocrypt.encrypt_file(filepath=self._creds_path, password=password if password else self.__password, shift=17)
 
     def unlock(self, password: str = None):
+        """
+Decrypts the credentials file on disk
+        :param password: The password to use (Will use the default pw for the current object unless a different on is specified)
+        :return: None
+        """
         if self.state() == 'unlocked':
             return
         try:
@@ -75,12 +110,21 @@ class Credentials:
             raise CredentialsLockedError('Unable to unlock credentials with the provided password.')
 
     def change_salt(self, salt_length: int):
+        """
+Changes the salt used to encrypt the credentials with.
+        :param salt_length: any integer length for the new salt
+        :return: None
+        """
         self.unlock()
         with open(self._salt_file, 'wb') as sf:
             sf.write(os.urandom(salt_length))
         self.lock()
 
     def engage(self):
+        """
+Loads the credentials into the environment as environment variables and sets up the global creds as attributes.
+        :return: None
+        """
         self.unlock()
         with open(self._creds_path, 'r') as cf:
             creds = json.load(cf)
@@ -90,11 +134,19 @@ class Credentials:
             os.environ[key] = value
 
     def disengage(self):
+        """
+Removes the credentials from the environment and the attributes.
+        :return: None
+        """
         for key in self.__store.env.keys():
             del os.environ[key]
         self.__store.clear()
 
     def write(self):
+        """
+Saves the local credentials to the disk.
+        :return: None
+        """
         self.unlock()
         data = {
             "ENV": {k: v for k, v in self.__store.env.items()},
@@ -105,6 +157,13 @@ class Credentials:
         self.lock()
 
     def update_item(self, key, new_value, scope: str = 'env'):
+        """
+Updates a password or credential in the given scope with a new value.
+        :param key: The name of the credential
+        :param new_value: The new value
+        :param scope: The scope where the credential lives, 'env' for environment variables or 'global' for attributes. Defaults to env.
+        :return: None
+        """
         if scope.lower() == 'env':
             self.__store.env[key] = new_value
         elif scope.lower() == 'global':
@@ -112,6 +171,13 @@ class Credentials:
         self.write()
 
     def add_item(self, key, value, scope: str = 'env'):
+        """
+Adds a new item to the credential manager. Will give an error if another key exists in the same scope with the same name.
+        :param key: The name of the new key
+        :param value: The value of the credential
+        :param scope: The scope where the credential lives, 'env' for environment variables or 'global' for attributes. Defaults to env.
+        :return: None
+        """
         if scope.lower() == 'env':
             if self.__store.env.get(key, False):
                 raise CredentialOverrideError(f'{key} already exists in the environment credentials')
