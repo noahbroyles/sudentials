93eb8ac02372fd0d5e4015915a5db19b620d5d13
Noah Broyles <noah@javamate.net>
2022-01-07 14:29:53 -0500

    There is no cipher

--- robocrypt.py
+++ robocrypt.py
@@ -16,13 +16,6 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 # This is the version of the programn ;)
 __version__ = '4Pro'
 
-CIPHER_WHEEL_N = {0: b'A', 1: b'B', 2: b'C', 3: b'D', 4: b'E', 5: b'F', 6: b'G', 7: b'H', 8: b'I', 9: b'J', 10: b'K', 11: b'L', 12: b'M', 13: b'N', 14: b'O', 15: b'P', 16: b'Q', 17: b'R', 18: b'S', 19: b'T', 20: b'U', 21: b'V', 22: b'W', 23: b'X', 24: b'Y', 25: b'Z',
-                  26: b'a', 27: b'b', 28: b'c', 29: b'd', 30: b'e', 31: b'f', 32: b'g', 33: b'h', 34: b'i', 35: b'j', 36: b'k', 37: b'l', 38: b'm', 39: b'n', 40: b'o', 41: b'p', 42: b'q', 43: b'r', 44: b's', 45: b't', 46: b'u', 47: b'v', 48: b'w', 49: b'x', 50: b'y',
-                  51: b'z', 52: b'0', 53: b'1', 54: b'2', 55: b'3', 56: b'4', 57: b'5', 58: b'6', 59: b'7', 60: b'8', 61: b'9', 62: b'+', 63: b'/', 64: b'=', 65: b'_', 66: b'-'}
-CIPHER_WHEEL_L = {b'A': 0, b'B': 1, b'C': 2, b'D': 3, b'E': 4, b'F': 5, b'G': 6, b'H': 7, b'I': 8, b'J': 9, b'K': 10, b'L': 11, b'M': 12, b'N': 13, b'O': 14, b'P': 15, b'Q': 16, b'R': 17, b'S': 18, b'T': 19, b'U': 20, b'V': 21, b'W': 22, b'X': 23, b'Y': 24, b'Z': 25,
-                  b'a': 26, b'b': 27, b'c': 28, b'd': 29, b'e': 30, b'f': 31, b'g': 32, b'h': 33, b'i': 34, b'j': 35, b'k': 36, b'l': 37, b'm': 38, b'n': 39, b'o': 40, b'p': 41, b'q': 42, b'r': 43, b's': 44, b't': 45, b'u': 46, b'v': 47, b'w': 48, b'x': 49, b'y': 50,
-                  b'z': 51, b'0': 52, b'1': 53, b'2': 54, b'3': 55, b'4': 56, b'5': 57, b'6': 58, b'7': 59, b'8': 60, b'9': 61, b'+': 62, b'/': 63, b'=': 64, b'_': 65, b'-': 66}
-
 
 def get_salt(salt_file='/var/secure/robocrypt.salt'):
     with open(salt_file, 'rb') as sf:
